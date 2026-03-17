4b0bd82e6f2e21b86b96383ef67019d87ea44c93
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-17 11:52:59 +0300

    post

--- client/src/Pages/Posts.jsx
+++ client/src/Pages/Posts.jsx
@@ -2,8 +2,67 @@ import React from 'react'
 
 const Posts = () => {
   return (
-    <div>Posts</div>
+    <div className='flex flex-col items-center overflow-y-scroll max-w-6xl'>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
+    </div>
   )
 }
 
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, similique?</h1>
 export default Posts
