8501fcc026821a21ec764380ea549db7ebf58f8d
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-17 06:14:46 +0300

    some padding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Nav from './Components/Nav'
 import Home from './Pages/Home'
+import Posts from './Pages/Posts'
+import Profile from './Pages/Profile'
 
 const App = () => {
   return (
     <div>
 
       <Nav/>
-      <Routes>
+      <div className='px-5 md:px-20 pt-5'>
+        <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/posts' element={<Posts/>}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
+      </div>
     </div>
   )
 }
